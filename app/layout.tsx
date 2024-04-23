d5530bd4b834c4fbdb5324e0a1b40e2256262fd4
Verifieddanny <dannyclassic56@gmail.com>
2024-04-23 22:54:22 +0100

    Fix[minor changes]

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DevDanny",
   description: "Replica of Dennis's website ",
+  metadataBase: new URL("https://next-dashboard.pinkjelly.org/"),
   openGraph: {
     title: "DevDanny",
     description: "Replica of Davis Website",
